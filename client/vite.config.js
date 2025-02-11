84d2e805fb3d3dcd207e4432fd438373633ddb19
abeer_arora <abeerarora51017@gmail.com>
2025-02-11 17:03:46 +0530

    w

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
+
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/file-converter/', // Add this line
+  plugins: [react()],
+  base: '/file-converter/',  // Add this line
 });
